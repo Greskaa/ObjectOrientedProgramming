efa3c03f7181b9e6ef96cbc34f695510c12de244
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2022-06-12 21:38:29 +0200

    Update Solution2.cpp

--- Laboratory_Exercises/Lab_3/Solution2.cpp
+++ Laboratory_Exercises/Lab_3/Solution2.cpp
@@ -1,98 +1,92 @@
-#include <iostream>
-#include <cstring>
+    #include <iostream>
+    #include <cstring>
 
-using namespace std;
+    using namespace std;
 
-class Worker
-{
-private:
-    char firstName[30];
-    char lastName[30];
-    int salary;
-public:
-    //2 in 1 Constructor
-    Worker(char *name="", char *surname="", int salary=0)
+    class Worker
     {
-        strcpy(this->firstName, name);
-        strcpy(this->lastName, surname);
-        this->salary = salary;
-    }
-
-    int getSalary()
-    {
-        return  salary;
-    }
-
-    void print()
-    {
-        cout<<firstName<<" "<<lastName<<" "<<salary<<endl;
-    }
-};
-
-class Factory
-{
-private:
-    int workersNumber;
-    Worker workers[100];
-public:
-    //Constructor with arguments
-    Factory(int workersNumber, Worker *workers)
-    {
-        this->workersNumber = workersNumber;
-        for(int i=0; i<workersNumber; i++)
-        {
-            this->workers[i] = workers[i];
-        }
-    }
-
-    //All workers
-    void printWorkers()
-    {
-        for (int i=0; i<workersNumber; i++)
-        {
-            workers[i].print();
-        }
-    }
-
-    //Depending on salary
-    void printWithSalary(int salary)
-    {
-        for (int i=0; i<workersNumber; i++)
-        {
-            if (workers[i].getSalary() > salary)
-            {
-                workers[i].print();
-            }
-        }
-    }
-};
-
-int main()
-{
-    int n;
-    cin>>n;
-
-    Worker *workers = new Worker[n];
-
-    //Print All Workers
-    cout<<"ALL WORKERS: "<<endl;
-    for (int i=0; i<n; i++)
-    {
+    private:
         char firstName[30];
         char lastName[30];
         int salary;
-        cin>>firstName>>lastName>>salary;
-        workers[i] = Worker(firstName, lastName, salary);
+    public:
+        //Default Constructor
+        Worker(){}
+        //Constructor with arguments
+        Worker(char *firstName, char *lastName, int salary)
+        {
+            strcpy(this->firstName, firstName);
+            strcpy(this->lastName, lastName);
+            this->salary = salary;
+        }
+
+        int getSalary()
+        {
+            return salary;
+        }
+
+        void print()
+        {
+            cout<<firstName<<" "<<lastName<<" "<<salary<<endl;
+        }
+    };
+
+    class Factory
+    {
+    private:
+        int workersNumber;
+        Worker workers[100];
+    public:
+        Factory(int workersNumber, Worker *workers)
+        {
+            this->workersNumber = workersNumber;
+            for (int i=0; i<workersNumber; i++)
+            {
+                this->workers[i] = workers[i];
+            }
+        }
+
+        void printWithSalary(int salary)
+        {
+            for (int i=0; i<workersNumber; i++)
+            {
+                if (workers[i].getSalary() > salary)
+                  {
+                        workers[i].print();
+                  }
+            }
+        }
+    };
+
+    int main()
+    {
+        int n;
+        cin>>n;
+        Worker workers[n];
+
+        for (int i=0; i<n; i++)
+        {
+            char name[30];
+            char surname[30];
+            int salary;
+            cin>>name>>surname>>salary;
+            workers[i] = Worker(name, surname, salary);
+
+        }
+
+        cout<<"ALL WORKERS: "<<endl;
+        for (int i=0; i<n; i++)
+        {
+            workers[i].print();
+        }
+
+        int minSalary;
+        cin>>minSalary;
+
+        Factory factory(n, workers);
+        cout<<"EMPLOYEES WITH SALARY "<<minSalary<<":"<<endl;
+
+        factory.printWithSalary(minSalary);
+
+        return 0;
     }
-
-    int minSalary;
-    cin>>minSalary;
-
-    //Print based on salary
-    Factory fact(n, workers);
-    fact.printWorkers();
-    cout<<"EMPLOYEES WITH SALARY "<<minSalary<<":"<<endl;
-    fact.printWithSalary(minSalary);
-
-    return 0;
-}
